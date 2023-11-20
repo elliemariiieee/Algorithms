04a2e7714e68f512cdba6ddf7943939dc5c4d193
elliemariiieee <399384@colum.edu>
2023-11-19 21:17:48 -0600

    added bubble sort

---
+++ Sorting/Sorting/Bubble.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public class Bubble
+    {
+        public List<int> Run(List<int> ints)
+        {
+            List<int> result = ints;
+            bool notSorted = true;
+            while (notSorted)
+            {
+                notSorted = false;
+                for(int i = 1;  i < result.Count; i++)
+                {
+                    if (ints[i] < ints[i - 1])
+                    {
+                        int temp = result[i];
+                        result[i] = result[i - 1];
+                        result[i - 1] = temp;
+
+                        notSorted = true;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                    continue;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Sorting/Sorting/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Timers;
+
+namespace Sorting
+{
+    internal class Program
+    {
+        private static System.Diagnostics.Stopwatch watch;
+        static void Main(string[] args)
+        {
+            Bubble b = new Bubble();
+            Heap h = new Heap();
+            Insertion i = new Insertion();
+            Merge m = new Merge();
+            Quick q = new Quick();
+            Selection s = new Selection();
+
+            //gets input from external file
+            List<string> files = new List<string>();
+            List<string> lines = new List<string>();
+            string path = "..\\..\\..\\Files";
+            StreamReader reader;
+            files = Directory.GetFiles(path).ToList();
+            for (int x = 0; x < files.Count; x++)
+            {
+                reader = new StreamReader(files[x]);
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+            }
+
+            //converts list from string to int
+            List<int> scores = new List<int>();
+            scores = lines.Select(int.Parse).ToList();
+
+            watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+            b.Run(scores);
+            watch.Stop();
+            Console.WriteLine($"Bubble Sort is an algorithm that compares the current value to the previous one and repeats until all values are sorted. It sorted the data in {watch.ElapsedMilliseconds} ms. Psuedocode for the alogrithm follows:");
+            Console.WriteLine("Bubblesort(Data: values[])\n  Boolean: not_sorted = True\n  While(not_sorted)\n    not_sorted = False\n    For i = 0 To <length of values> - 1\n      If(values[i] < values[i-1]) Then\n        Data: temp = values[i]\n        values[i] = values[i-1]\n        values[i-1] = temp\n        not_sorted = True\n      End If\n    Next i\n  End While\nEnd Bubblesort");
+            
+        }
+
+    }
+}
